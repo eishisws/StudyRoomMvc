c39a41afe15ee96d3041438f4a5b700152d067af
Eishi Smith <2231400@cegep-heritage.qc.ca>
2025-09-26 14:55:43 -0400

    Initial Migration and DB Setup

--- StudyRoomMvc/obj/Debug/net8.0/StudyRoomMvc.AssemblyInfo.cs
+++ StudyRoomMvc/obj/Debug/net8.0/StudyRoomMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyRoomMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdb06473b2f170748a99d4f864ec19e36c783cba")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyRoomMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyRoomMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
